f6896220919eacc78d2830b72f035efe7383dc26
hosam175 <hosamsafar50@gmail.com>
2021-01-20 15:04:58 +0200

    adding webhoker verifiy

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ app.listen(app.get('port'), function () {
     res.send("Hey I am Here");
   });
   
+  app.get("/webhook", function (req, res) {
+
+    const PAGE_VERIFY_TOKEN = "ourchatbotwebapp1234";
+  
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+  
+    if (token === PAGE_VERIFY_TOKEN) {
+  
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+  
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  });
+  
