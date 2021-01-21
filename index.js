8cfcc65e589beb0e0767e8e57801761e5fbc5733
hosam175 <hosamsafar50@gmail.com>
2021-01-21 03:33:41 +0200

    change  meesenger url

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 const request = require("request");
 
 const PAGE_ACCESS_TOKEN ="EAAJ8qdCTQo4BAFFoOrN1nW1bsw3XTqR3f0BCgfCJohp6eYRPN7ZAUIBu7QxxRAZAZCxWOLD9T3c18nOwVAt1cu4qAOTuq8rWlu5cFoDnY5RNUsbQ2VWvAE3cACUbgHOcvEwZAyrs9Rs6LiZA9QDwZCK9hP3jgcj9qw2UHQOoxbpMvKH2SKVhqJXZAYrYVZCMxHgZD"
-const api_url = "https://graph.facebook.com/v9.0/me/messages?access_token=<PAGE_ACCESS_TOKEN>"
+const api_url = "https://graph.facebook.com/v9.0/me/messenger_profile?access_token=<PAGE_ACCESS_TOKEN>
+"
 // app configuration
 app.set('port', (process.env.PORT || 8000));
 
@@ -27,20 +28,19 @@ app.listen(app.get('port'), function () {
 
   app.get("/setup", function (req, res) {
  
-    var data =[
-      { 
-        "get_started":{
-          "payload":"<GET_STARTED_PAYLOAD>"
+    var data ={ 
+        "greeting":[
+          {
+          "local":"defult",
+          "text":"Heloid"
         }
-      }
-
-    ]
-  });
+      ]
+  };
   
   request(
     {
 
-       url : "https://graph.facebook.com/v9.0/me/messages?access_token=" +PAGE_ACCESS_TOKEN ,
+       url : "https://graph.facebook.com/v9.0/me/messenger_profile?access_token=" + PAGE_ACCESS_TOKEN ,
        method :"POST" ,
        headers :{ "Content-Type": "application/json"},
        form : data
