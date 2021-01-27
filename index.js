e9d68c768ecbbfd98a016b28827c2f119d48903a
hosam175 <hosamsafar50@gmail.com>
2021-01-27 19:42:12 +0200

    ok

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ function receivedPostback(event) {
   var senderid = event.sender.id;
   var payload = event.postback.payload;
   switch (payload) {
-    case "Get Started":
+    case "GET_STARTED_PAYLOAD":
       var msg = "تعتبر مؤسستنا واحدة من أهم مؤسسات انتاج وتسويق الملبوسات"
       sendTextMessage(senderid, msg)
       var data = {
